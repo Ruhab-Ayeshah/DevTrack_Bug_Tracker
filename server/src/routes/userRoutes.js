1eec227ffce25fec78448246c68f7d3adc4e2287
Ruhab Ayeshah <ruhab.ayeshah2212@gmail.com>
2026-04-25 16:04:18 +0500

    Added userSearch endpoint

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {getAllUsers, getUserById, updateUser, deleteUser} from '../controllers/userController.js';
+import {getAllUsers, getUserById, updateUser, deleteUser, searchUsers} from '../controllers/userController.js';
 
 import {authMiddleware} from '../middleware/authMiddleware.js';
 import roleMiddleware from '../middleware/roleMiddleware.js';
@@ -8,6 +8,8 @@ import roleMiddleware from '../middleware/roleMiddleware.js';
 const router = express.Router();
 
 router.get("/", authMiddleware, roleMiddleware(["admin"]), getAllUsers);
+
+router.get("/search", authMiddleware, searchUsers);
 
 router.get("/:id", authMiddleware, getUserById);
 
